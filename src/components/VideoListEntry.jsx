cf62eaf31f362f391a4205e2c8fc5d453d1506e1
Hack Reactor Student <student@hackreactor.com>
2017-09-08 20:12:36 -0700

    implements videolist, videoplayer, and clicks are registering

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,29 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  onVideoClick() {
+    var newIndex = this.props.index;
+    this.setState({
+      current: {newIndex}
+    });
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.vid.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title" onClick={this.onVideoClick.bind(this)}>{this.props.vid.snippet.title}</div>
+          <div className="video-list-entry-detail">{this.props.vid.snippet.description}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
